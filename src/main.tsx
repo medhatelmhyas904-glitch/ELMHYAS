3eaa5c71bd8bc87e81dfe43386b7561f3e9094bd
medhatelmhyas904-glitch <medhatelmhyas904@gmail.com>
2025-08-14 05:05:36 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1 style={{ color: 'red', fontSize: '48px', margin: '50px', textAlign: 'center' }}>
-      Test Successful!
-    </h1>
+    <App />
   </React.StrictMode>,
 )
